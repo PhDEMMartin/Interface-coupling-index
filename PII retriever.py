cf90d2fe35ff5d29a61ae49d2e6787650e7f92ca
PhDEMMartin <bio.eng.emmartin@outlook.com>
2022-01-07 19:39:54 -0600

    Updating two documents
    updated changes from both files that were required

--- PII retriever.py
+++ PII retriever.py
@@ -42,25 +42,30 @@ def read_file_2(file_name_2):
 
     # print(pii_matrix)
     for i in range(len(pii_matrix)):
-        pii.append(int(pii_matrix[i][index]))
-    # print(pii)
+        if pii_matrix[i][index] != '-':
+            pii.append(int(pii_matrix[i][index]))
+        else:
+            pii.append('NA')
+    print(pii)
 
 
 def retriever():
     np.zeros((len(pii), len(pii)))
-    # print(len(mip_matrix))
+    print('MIp matrix length', len(mip_matrix))
     last_position = int(mip_matrix[(len(mip_matrix))-1][0])
-    # print(last_position)
-    start = ((pii[0] * (last_position - 1))-1195)
-    # print(start)
-    # print(mip_matrix[start][0])
+    print('last position', last_position)
+    # start = ((pii[0] * (last_position - 1))-1195)
+    start = ((pii[0] * (last_position - 1))-(last_position-1))
+    print('start', start)
+    print(mip_matrix[start][0])
     end = (pii[len(pii) - 1] * (last_position - 1))
-    # print(end)
+    print('last pii', pii[len(pii) - 1])
+    print('end', end)
 
     # for i in range(630960, 632150):
     #     print(mip_matrix[i])
 
-    save = '{}wMIp_test_4.csv'.format(file[:6])
+    save = '{}wMIp_final_4.csv'.format(file[:6])
     f = open(save, "a")
     header_2 = '{},{},{},{},{},{},{},{},{},{}\n'.format(header[0], header[1], header[2], header[3], header[4],
                                                         header[5], header[6], header[7], header[8], header[9])
@@ -70,20 +75,66 @@ def retriever():
         for j in range(len(pii)):
             for k in range(len(pii)):
                 # print(mip_matrix[i][0], mip_matrix[i][1], pii[j], pii[k])
-                if int(mip_matrix[i][0]) == int(pii[j]) and int(mip_matrix[i][1]) == int(pii[k]):
-                    # print(mip_matrix[i])
+                if pii[j] != 'NA' and pii[k] != 'NA':
+                    if int(mip_matrix[i][0]) == int(pii[j]) and int(mip_matrix[i][1]) == int(pii[k]):
+                        # print(mip_matrix[i])
 
-                    line = '{},{},{},{},{},{},{},{},{},{}\n'.format(mip_matrix[i][0], mip_matrix[i][1],
-                                                                    mip_matrix[i][2], mip_matrix[i][3],
-                                                                    mip_matrix[i][4], mip_matrix[i][5],
-                                                                    mip_matrix[i][6], mip_matrix[i][7],
-                                                                    mip_matrix[i][8], mip_matrix[i][9])
+                        line = '{},{},{},{},{},{},{},{},{},{}\n'.format(mip_matrix[i][0], mip_matrix[i][1],
+                                                                        mip_matrix[i][2], mip_matrix[i][3],
+                                                                        mip_matrix[i][4], mip_matrix[i][5],
+                                                                        mip_matrix[i][6], mip_matrix[i][7],
+                                                                        mip_matrix[i][8], mip_matrix[i][9])
+                        f.write(line)
+                # elif pii[j] == 'NA' and pii[k] != 'NA':
+                #     if int(mip_matrix[i][1]) == int(pii[k]):
+                #         line = '{},{},{},{},{},{},{},{},{},{}\n'.format('NA', mip_matrix[i][1],
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA')
+                #         f.write(line)
+                # elif pii[j] != 'NA' and pii[k] == 'NA':
+                #     if int(mip_matrix[i][0]) == int(pii[j]):
+                #         line = '{},{},{},{},{},{},{},{},{},{}\n'.format(mip_matrix[i][0], 'NA',
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA',
+                #                                                         'NA', 'NA')
+                #         f.write(line)
+                else:
+                    pass
+    for jj in range(0, len(pii)):
+        for kk in range(0, len(pii)):
+            if jj != kk:
+                if pii[kk] == 'NA':
+                    line = '{},{},{},{},{},{},{},{},{},{}\n'.format(pii[jj], 'NA',
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA')
                     f.write(line)
+                elif pii[jj] == 'NA':
+                    line = '{},{},{},{},{},{},{},{},{},{}\n'.format('NA', pii[kk],
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA',
+                                                                    'NA', 'NA')
+                    f.write(line)
+                else:
+                    pass
+
+    for ii in range(0, len(pii)):
+        line = '{},{},{},{},{},{},{},{},{},{}\n'.format(pii[ii], pii[ii],
+                                                        'NA', 'NA',
+                                                        'NA', 'NA',
+                                                        'NA', 'NA',
+                                                        'NA', 'NA')
+        f.write(line)
     f.close()
 
 
 global file
-file = 'NarQP__MIp_detailed_4.csv'
+file = 'QseCB__MIp_detailed_4.csv'
 read_file(file)
 read_file_2('pii_test.csv')
 retriever()
